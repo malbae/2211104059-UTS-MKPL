e37e9d78880be4845d140d632c02c7d8e7f68785
malbae <136548472+malbae@users.noreply.github.com>
2025-04-16 16:59:08 +0700

    Create Spouse.java newparametertomethodXYZ

---
+++ TaxCalculator/src/lib/Spouse.java
@@ -0,0 +1,19 @@
+package lib;
+
+public class Spouse {
+	private String name;
+	private String idNumber;
+
+	public Spouse(String name, String idNumber) {
+		this.name = name;
+		this.idNumber = idNumber;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public String getIdNumber() {
+		return idNumber;
+	}
+}
